997d50ec4c9db7870c3c670480d56e4e43a62e20
Jiaxinhuang1 <60531514+Jiaxinhuang1@users.noreply.github.com>
2022-09-19 17:56:45 -0500

    added how to play

--- DinoFeederRP1/Assets/Scripts/DinoUIController.cs
+++ DinoFeederRP1/Assets/Scripts/DinoUIController.cs
@@ -8,17 +8,22 @@ public class DinoUIController : MonoBehaviour
 {
     public GameObject titleText;
     public GameObject playButton;
+    public GameObject howButton;
     public GameObject quitButton;
+    public GameObject howPanel;
     // Start is called before the first frame update
     void Start()
     {
+        howPanel.SetActive(false);
         titleText.transform.localScale = new Vector3(0, 0, 0);
         playButton.transform.localScale = new Vector3(0, 0, 0);
+        howButton.transform.localScale = new Vector3(0, 0, 0);
         quitButton.transform.localScale = new Vector3(0, 0, 0);
 
         LeanTween.scale(titleText, new Vector3(1, 1, 1), 0.2f).setDelay(0.2f);
         LeanTween.scale(playButton, new Vector3(1, 1, 1), 0.3f).setDelay(0.4f);
-        LeanTween.scale(quitButton, new Vector3(1, 1, 1), 0.3f).setDelay(0.6f);
+        LeanTween.scale(howButton, new Vector3(1, 1, 1), 0.3f).setDelay(0.6f);
+        LeanTween.scale(quitButton, new Vector3(1, 1, 1), 0.3f).setDelay(0.8f);
     }
 
     // Update is called once per frame
@@ -41,7 +46,19 @@ public class DinoUIController : MonoBehaviour
     {
         LeanTween.scale(titleText, new Vector3(0, 0, 0), 0.3f).setDelay(0.2f);
         LeanTween.scale(playButton, new Vector3(0, 0, 0), 0.3f);
+        LeanTween.scale(howButton, new Vector3(0, 0, 0), 0.3f).setDelay(0.2f);
         LeanTween.scale(quitButton, new Vector3(0, 0, 0), 0.3f).setDelay(0.2f).setOnComplete(ChangeTileScene);
+    }
+
+    public void OpenHow()
+    {
+        howPanel.SetActive(true);
+        LeanTween.scale(howPanel, new Vector3(1, 1, 1), 0.3f);
+    }
+
+    public void CloseHow()
+    {
+        LeanTween.scale(howPanel, new Vector3(0, 0, 0), 0.3f).setOnComplete(()=> howPanel.SetActive(false));
     }
 
     public void RestartGame()
