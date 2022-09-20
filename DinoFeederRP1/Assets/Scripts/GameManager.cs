7fccc864c0c6a1378e1fb68a29336db31935aa88
Jiaxinhuang1 <60531514+Jiaxinhuang1@users.noreply.github.com>
2022-09-20 16:06:31 -0500

    fence setup but without art yet

--- DinoFeederRP1/Assets/Scripts/GameManager.cs
+++ DinoFeederRP1/Assets/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     public int mapSizeX, mapSizeY;
     public float tileSize;
     public GameObject tile;
+    public GameObject fence;
     public GameObject currentFossil;
     public GameObject player;
     public GameObject house;
@@ -46,22 +47,34 @@ public class GameManager : MonoBehaviour
         {
             for (int j = 0; j < mapSizeY; j++)
             {
-                GameObject newTile = Instantiate(tile, transform);
-                newTile.name = i + ", " + j;
-                newTile.transform.position = new Vector2(i * tileSize + tileSize/2, j * tileSize + tileSize/2);
-                tiles.Add(newTile);
+                if (i == 0 || i == mapSizeX - 1 || j == 0 || j == mapSizeY - 1)
+                {
+                    GameObject newFence = Instantiate(fence, transform);
+                    newFence.name = "Fence:" + i + ", " + j;
+                    newFence.transform.position = new Vector2(i * tileSize + tileSize / 2, j * tileSize + tileSize / 2);
+                    tiles.Add(newFence);
+                }
+                else
+                {
+                    GameObject newTile = Instantiate(tile, transform);
+                    newTile.name = i + ", " + j;
+                    newTile.transform.position = new Vector2(i * tileSize + tileSize / 2, j * tileSize + tileSize / 2);
+                    tiles.Add(newTile);
+                }
             }
         }
-        GameObject newHouse = Instantiate(house, tiles[19].transform.position, Quaternion.identity, transform);
-        GameObject newWell = Instantiate(well, tiles[8].transform.position, Quaternion.identity, transform);
-        tiles[19].GetComponent<TileBehaviour>().contains = newHouse;
+
+        GameObject newHouse = Instantiate(house, tiles[30].transform.position, Quaternion.identity, transform);
+        GameObject newWell = Instantiate(well, tiles[18].transform.position, Quaternion.identity, transform);
         tiles[18].GetComponent<TileBehaviour>().contains = newHouse;
-        tiles[17].GetComponent<TileBehaviour>().contains = newHouse;
-        tiles[9].GetComponent<TileBehaviour>().contains = newHouse;
-        tiles[8].GetComponent<TileBehaviour>().contains = newHouse;
-        tiles[7].GetComponent<TileBehaviour>().contains = newHouse;
+        tiles[19].GetComponent<TileBehaviour>().contains = newHouse;
+        tiles[20].GetComponent<TileBehaviour>().contains = newHouse;
+        tiles[40].GetComponent<TileBehaviour>().contains = newHouse;
+        tiles[41].GetComponent<TileBehaviour>().contains = newHouse;
+        tiles[42].GetComponent<TileBehaviour>().contains = newHouse;
         tiles[29].GetComponent<TileBehaviour>().contains = newHouse;
-        tiles[28].GetComponent<TileBehaviour>().contains = newHouse;
-        tiles[27].GetComponent<TileBehaviour>().contains = newHouse;
+        tiles[30].GetComponent<TileBehaviour>().contains = newHouse;
+        tiles[31].GetComponent<TileBehaviour>().contains = newHouse;
+        tiles[17].GetComponent<TileBehaviour>().contains = newHouse;
     }
 }
